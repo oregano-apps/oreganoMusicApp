f9880a1b6a08d0ffd2f939385e24fea5aa509203
Meir Shaish <almogmaimon674@gmail.com>
2021-08-07 18:30:04 +0300

    07/07/21

--- controllers/song.js
+++ controllers/song.js
@@ -1,10 +1,16 @@
 const catchAsync = require("./../utils/catchAsync");
 const appError = require("./../utils/appError");
 
+
+
+
 exports.createSong = catchAsync(async (req, res, next) => {
     console.log(req.body)
 })
 
 exports.uploadAudio = catchAsync(async (req, res, next) => {
-    console.log('require')
+    const audioPath = req
+    console.log('request arrived')
+    console.log(audioPath)
+    res.send({audioPath})
 })
--- routes/songs.js
+++ routes/songs.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const SpotifyWebApi = require("spotify-web-api-node")
 const songController = require('./../controllers/song')
 const spotifyController = require('./../controllers/spotify')
+const multer = require('multer')
+
+const upload = multer({ dest: 'images/' })
 
 router.post("/spotifyLogin", spotifyController.login_to_spotify)
 
 router.post("/refresh", spotifyController.getRefreshToken)
 
 router.post('/createSong', songController.createSong)
-router.post('/uploadAudio', songController.uploadAudio)
+router.post('/uploadAudio', upload.single('soundBlob'), songController.uploadAudio)
 
 module.exports = router;
